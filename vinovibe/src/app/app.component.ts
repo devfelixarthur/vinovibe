32f18f4172b85c66b7dbc227ca05f1306853843f
Arthur Felix <arthur.andrade@omnilink.com.br>
2024-07-10 14:25:29 -0300

    Configuração do projeto, criação dos components e implementação do roteamento

--- vinovibe/src/app/app.component.ts
+++ vinovibe/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './compnents/footer/footer.component';
+import { HeaderComponent } from './compnents/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'vinovibe';
--- vinovibe/src/app/app.routes.ts
+++ vinovibe/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './compnents/pages/home/home.component';
+import { LoginComponent } from './compnents/pages/login/login.component';
+import { AboutComponent } from './compnents/pages/about/about.component';
+import { AdminComponent } from './compnents/pages/admin/admin.component';
+import { RecoveryPasswordComponent } from './compnents/pages/recovery-password/recovery-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'recoveryPassword', component: RecoveryPasswordComponent},
+];
